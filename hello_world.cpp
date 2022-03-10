c0e0160765c9dfd75c2a9131493ba63444f6af35
nnnnnmmmmm092 <nnnnnmmmmm092@protonmail.com>
2022-03-10 12:26:48 +0300

    Comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 int main() 
-{
+{	//var uname contains username
 	char uname[50] = "";
+
+	// getting uname from input stream
 	std::cin >> uname;
+
+	//printing out
 	std::cout << "Hello,World! from  " <<uname << std::endl;
 }
