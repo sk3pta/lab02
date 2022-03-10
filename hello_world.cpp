6f7af39f2dac48ab29e81cfac2431aef12b0cdb4
nnnnnmmmmm092 <nnnnnmmmmm092@protonmail.com>
2022-03-10 12:45:02 +0300

    Mozilla Codestyle

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
-int main() 
-{	//var uname contains username
-	char uname[50] = "";
+int
+main()
+{ // var uname contains username
+  char uname[50] = "";
 
-	// getting uname from input stream
-	std::cin >> uname;
+  // getting uname from input stream
+  std::cin >> uname;
 
-	//printing out
-	std::cout << "Hello,World! from  " <<uname << std::endl;
+  // printing out
+  std::cout << "Hello,World! from  " << uname << std::endl;
 }
+
